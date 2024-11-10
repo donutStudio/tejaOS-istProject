c75b6988a5970373f0951da65df79cd3a6afbb14
donutStudio <79951682+donutStudio@users.noreply.github.com>
2024-11-10 18:10:03 -0500

    Delete TejaOS GUI directory

--- TejaOS GUI/reset.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'p' || event.key === 'P') {
-        event.preventDefault();
-        window.location.href = 'index.html';
-    }
-});
